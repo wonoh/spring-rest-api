c3aa635d70ec9495b1f7b113fef4b28a53b4370a
wonoh <wldnjsdh44121@nate.com>
2019-01-23 11:19:45 +0900

    Event domain

---
+++ src/main/java/me/wonoh/restapi/events/EventStatus.java
@@ -0,0 +1,5 @@
+package me.wonoh.restapi.events;
+
+public enum EventStatus {
+    DRAFT,PUBLISHED,BEGAN_ENROLLMEND,CLOSED_ENROLLMENT,STARTED,ENDED
+}
---
+++ src/test/java/me/wonoh/restapi/events/EventTest.java
@@ -0,0 +1,30 @@
+package me.wonoh.restapi.events;
+
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                .name("Spring rest api")
+                .description("rest api dev with spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+            Event event = new Event();
+            String name = "test";
+            String des = "rest";
+            event.setName(name);
+            event.setDescription(des);
+            assertThat(event.getName()).isEqualTo(name);
+            assertThat(event.getDescription()).isEqualTo(des);
+    }
+
+}
