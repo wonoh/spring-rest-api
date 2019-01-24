ddd1eb1af62ba0f3e3f588db6195f91faa26d12a
wonoh <wldnjsdh44121@nate.com>
2019-01-24 14:55:22 +0900

    add Entity annotation in event domain

--- src/main/java/me/wonoh/restapi/events/Event.java
+++ src/main/java/me/wonoh/restapi/events/Event.java
@@ -2,11 +2,15 @@ package me.wonoh.restapi.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter@Setter @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
+    @Id
+    @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -20,5 +24,6 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 }
