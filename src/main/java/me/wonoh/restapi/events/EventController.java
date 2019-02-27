3cc5c5289bdcfb2cc4dc5efd5ccf852f6e5b759d
wonoh <wldnjsdh44121@nate.com>
2019-02-27 12:54:16 +0900

    입력값 유효판단

--- src/main/java/me/wonoh/restapi/events/EventController.java
+++ src/main/java/me/wonoh/restapi/events/EventController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -28,7 +30,11 @@ public class EventController {
     }*/
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto){
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+
+        }
         Event event = modelMapper.map(eventDto,Event.class);
         Event newEvent = this.eventRepository.save(event);
         System.out.println(newEvent.toString());
--- src/test/java/me/wonoh/restapi/events/EventControllerTests.java
+++ src/test/java/me/wonoh/restapi/events/EventControllerTests.java
@@ -86,4 +86,14 @@ public class EventControllerTests {
                 .andExpect(status().isBadRequest())
         ;
     }
+
+    @Test
+    public void createdEvent_Bad_Request_Empty_Input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+        this.mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(this.objectMapper.writeValueAsString(eventDto))
+                )
+                .andExpect(status().isBadRequest());
+    }
 }
